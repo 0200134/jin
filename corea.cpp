9399c7993b9a06aa2c7b4fc243bb8ec842966a55
0200134 <84560987+0200134@users.noreply.github.com>
2023-07-06 19:51:30 +0900

    Corea( Intergated)

---
+++ corea.cpp
@@ -0,0 +1,1353 @@
+#include<iostream>
+
+#include<cstring>
+namespace Integration{
+ class Korea_Peninsula
+{
+	public:
+ 
+	class North_korea
+	{
+		class Chagang
+		{
+			struct Feature
+			{
+				char code[6];
+				double Area;
+			};
+
+		};
+		
+		class Hamgyoung
+		{
+			class North_Hamgyoung
+			{
+				struct Feature
+				{
+					char code[6];
+					double Area;
+				};
+			};
+			class South_Hamgyoung
+			{
+				struct Feature
+				{
+					char code[6];
+					
+					double Area;	
+				};
+			};
+		};
+		
+		class Hwanghae
+		{
+			class North_Hwanghae
+			{
+				struct Feature
+				{
+					char code[6];
+					double Area;
+					
+				};	
+							
+			};
+			class South_Hwanghae
+			{
+				struct Feature
+				{
+					char code[6];
+					double Area;	
+				};
+			};
+		};
+		class Kangwon
+		{
+			struct Feature
+			{
+				char code[6];
+				double Area;	
+			};	
+		};
+		class Pyongyang
+		{
+			class North_Pyongyang
+			{
+				struct Feature
+				{
+					char code[6];
+					double Area;	
+				};
+			};
+			class South_Pyoungan
+			{
+				char code[6];
+				double Area;
+			};
+		};
+		class Ryanggnag
+		{
+			struct Feature
+			{
+				char code[6];
+				double Area;	
+			};
+		};
+	};	
+	class South_korea
+	{
+
+		class Gyeonggido
+		{
+				class County
+				{
+					class Gapyeong
+					{
+							struct Feature
+							{
+									double Area = 843.46;
+							};
+					};
+					class Yangpyeong
+					{
+							struct Feature
+							{
+									double Area = 877.08;
+							};
+					};
+			
+					class Yeoncheon
+					{
+							struct Feature
+							{
+									double Area = 695.61;
+							};
+					};
+				};
+				class Suwon
+				{
+						struct Feature
+						{
+							double Area=121.09;
+						};
+				};
+				class Ansan
+				{
+						struct Feature
+						{
+								double Area=144.78;
+						};
+				};
+				class Anseong
+				{
+						struct Feature
+						{
+								double Area=553.46;
+						};
+				};
+				class Anyang
+				{
+						struct Feature
+						{
+								double Area=58.52;
+						};
+				};
+				class Bucheon
+				{
+						struct Feature
+						{
+							double Area=53.44; 
+						};
+				};
+				class Dongducheon
+				{
+						struct Feature
+						{
+								double Area=95.66;
+						};
+				};
+				class Gimpo
+				{
+						struct Feature
+						{
+								double Area=276.64;
+						};
+				};
+				class Goyang
+				{
+						struct Feature
+						{
+							double Area= 267.31;							
+						};
+				};
+				class Gunpo
+				{
+						struct Feature
+						{
+								double Area=36.38;
+						};
+				};
+				class Guri
+				{
+					struct Feature
+					{
+							double Area=33.3;
+					};
+				};
+				class Gwacheon
+				{
+						struct Feature
+						{
+								double Area=35.81;
+						};
+				};
+				class Gwangju
+				{
+						struct Feature
+						{
+							double Area= 430.69;
+						};
+				};
+				class Gwangmyeong
+				{
+						struct Feature
+						{
+							double Area=38.5;
+						};
+				};
+				class Hanam
+				{
+						struct Feature
+						{
+								double Area=93.07;
+						};
+				};
+				class Hwaseong
+				{
+						struct Feature
+						{
+								double Area= 844;
+						};
+				};
+				class Incheon
+				{
+					struct Feature
+					{
+							double Area=461.2;
+					};
+				};
+				class Namyangju
+				{
+						struct Feature
+						{
+							double Area=458.44;
+						};
+				};
+				class OSan
+				{
+						struct Feature
+						{
+								double Area = 42.76;
+						};
+				};
+				class Paju
+				{
+						struct Feature
+						{
+								double Area = 672.42;
+						};
+				};
+				class Pocheon
+				{
+						struct Feature
+						{
+							double Area = 826.38;
+						};
+				};
+				class Pyeongtaek
+				{
+						struct Feature
+						{
+								double Area = 452.18;
+
+						};
+				};
+				class Seongnam
+				{
+						struct Feature
+						{
+							double Area = 141.82;
+						};
+				};
+				class Siheung
+				{
+					struct Feature
+					{
+							double Area= 16.6;
+					};
+				};
+				class Uijeongbu
+				{
+						struct Feature
+						{
+								double Area = 81.59; 
+						};
+				};
+				class Uiwang
+				{
+						struct Feature
+						{
+								double Area = 53.95;
+						};
+
+				};
+				class Yangju
+				{
+						struct Feature
+						{
+								double Area=310.34;
+
+						};
+						
+				};
+				class Yeoju
+				{
+					struct Feature
+					{
+							double Area=607.79;
+					};
+				};
+				class Yongin
+				{
+						struct Feature
+						{
+								double Area = 591.32;
+						};
+				};
+		};
+		class special
+		{
+		class Seoul
+		{
+            class District
+            {
+                    class Dobonggu
+                    {
+                           struct Feature
+                           {
+                                double Area=20.70;
+                           };
+                    };
+                    class Dongdaemungu
+                    {
+                           struct Feature
+                           {
+                                double Area=14.21;
+                           };
+
+                    };
+                    class Dongjakgu
+                    {
+                            struct Feature
+                            {
+                                    double Area=16.35;
+                            };
+
+                    };
+                    class Eunpyeonggu
+                    {
+                            struct Feature
+                            {
+                                    double Area=29.7;
+
+                            };
+                    };
+
+                    class Gangbukgu
+                    {
+                           struct Feature
+                           {
+                                double Area=23.6;
+
+                           };
+                    };
+                    class Gangdonggu
+                    {
+                           struct Feature
+                           {
+                            double Area=24.59;
+                           };
+                    };
+                    class Gangnamgu
+                    {
+                           struct Feature
+                           {
+                            double Area=39.5;
+                           };
+                    };
+                    class Gangseogu
+                    {
+                           struct Feature
+                           {
+
+                                double Area=41.43;
+
+                           };
+                    };
+                    class Geumcheongu
+                    {
+                            struct Feature
+                            {
+                                double Area=13.02;
+                            };
+                    };
+                    class Gurogu
+                    {
+                                struct Feature
+                                {
+                                    double Area=20.12;
+                                };
+                    };
+                    class Gwanakgu
+                    {
+                            struct Feature
+                            {
+                                    double Area=29.57;
+                            };
+                    };
+                    class Gwangjingu
+                    {
+                           struct Feature
+                           {
+
+                                double Area=17.06;
+
+                           };
+                    };
+                    class Jongnogu
+                    {
+                           struct Feature
+                           {
+                                double Area=23.91;
+                           };
+                    };
+                    class Junggu
+                    {
+                            struct Feature
+                            {
+                                    double Area=9.96;
+                            };
+                    };
+                    class Jungnanggu
+                    {
+                            struct Feature
+                            {
+                                    double Area=18.5;
+                            };
+                    };
+                    class Mapogu
+                    {
+                        struct Feature
+                        {
+                            double Area=23.84;
+                        };
+                    };
+                    class Nowongu
+                    {
+                            double Area=35.44;
+                    };
+                    class Seochogu
+                    {
+                            double Area=47;
+                    };
+                    class Seodaemungu
+                    {
+                            struct Feature
+                            {
+
+                                double Area=17.61;
+
+                            };
+
+                    };
+                    class Seoungbukgu
+                    {
+                        struct Feature
+                        {
+                                double Area=24.58;
+                        };
+                    };
+                    class Seongdonggu
+                    {
+                        struct Feature
+                        {
+                                double Area=16.86;
+                        };
+                    };
+                    class Songpagu
+                    {
+                            struct Feature
+                            {
+                                double Area=33.88;
+                            };
+                    };
+                    class Yangcheongu
+                    {
+                            struct Feature
+                            {
+                                double Area=17.4;
+                            };
+                    };
+                    class Yeoungdeungpogu
+                    {
+                        struct Feature
+                        {
+                            double Area=24.53;
+                        };
+                    };
+                    class Yongsangu
+                    {
+                        struct Feature
+                        {
+
+                            double Area=21.87;
+
+                        };
+                    };
+
+            };
+			struct Feature
+			{
+				char code[6];
+				double Area;
+				
+			};
+			
+		};
+		
+		class Gangwon
+		{
+
+			struct Feature
+			{
+				char code[6];
+				double Area;
+			};
+
+			class County
+			{
+				class Hongcheon
+				{
+
+				};
+				class Cheonwon
+				{
+
+				};
+				class Hoengseong
+				{
+
+				};
+				class Pyeongchang
+				{
+
+				};
+				class Jeongseon
+				{
+
+				};
+				class Yeongwol
+				{
+
+				};
+				class  Inje
+				{
+
+				};
+				class Goseong
+				{
+
+				};
+				class Yangyang
+				{
+
+				};
+				class Hwacheon
+				{
+
+				};
+				class Yanggu
+				{
+
+				};
+			};
+			class Wonju
+			{
+
+			};
+			class Chuncheon
+			{
+
+			};
+			class Gangneung
+			{
+
+			};
+			class Donghae
+			{
+
+			};
+			class Sokcho
+			{
+
+			};
+			class samcheok
+			{
+
+			};
+			class Taebaek
+			{
+
+			};
+		};
+		class Jeju
+		{
+			struct Feature
+			{
+				char code[6];
+				double Area;
+					
+			};
+		};
+		class Sejong
+		{
+			struct Feature
+			{
+				char code[6];
+				double Area;	
+			};
+		};
+		
+			
+		};
+		class Metropolitan
+		{
+			class Busan
+			{
+				struct Feature
+				{
+				
+				char code[6];
+				double Area;
+				};
+				class Bukgu
+				{
+
+				};
+				class Busanjingu
+				{
+
+				};
+				class Donggu
+				{
+
+				};
+				class Dongnaegu
+				{
+
+				};
+				class Gangseogu
+				{
+				
+				};
+				class Geumjeonggu
+				{
+					
+				};
+
+				class Haeundaegu
+				{
+
+				};
+				class Jungnanggu
+				{
+
+				};
+				class Namgu
+				{
+
+				};
+				class Sahagu
+				{
+
+				};
+				class Sasanggu
+				{
+
+				};
+				class Seogu
+				{
+
+				};
+				class Suyeonggu
+				{
+
+				};
+				class Yeongdogu
+				{
+
+				};
+				class Yeonjegu
+				{
+
+				};
+				class Gijanggun
+				{
+
+				};
+			};
+			class Daegu
+			{	
+				struct Feature
+				{
+				
+				char code[6];
+				double Area;
+				};
+					class County
+					{
+						class Dalseong
+						{
+
+						};
+						class Gunwi
+						{
+
+						};
+					};
+					
+				class District
+				{
+					class Jung
+					{
+
+					};
+					class Dong
+					{
+
+					};
+					class Seo
+					{
+
+					};
+					class Nam
+					{
+
+					};
+					class Buk
+					{
+
+					};
+					class Suseong
+					{
+
+					};
+					class Dalseo
+					{
+
+					};
+					
+				};
+			};
+			class Incheon
+			{	struct Feature
+				{
+				char code[6];
+				double Area;
+				};
+			};
+			class Gwanju
+			{
+				struct Feature{
+				
+				char code[6];
+				double Area;
+				};
+			};
+			class Daejeon
+			{
+				struct Feature
+				{
+					char code[6];
+					double Area;
+					
+						
+				};
+			};
+			class Ulsan
+			{
+				char code[6];
+				double Area;	
+			};
+				
+		}; 
+		class Honam
+		{
+			class Jeolabukdo
+			{
+				struct Feature
+				{
+				
+				char code[6];
+				
+				double Area;
+				
+				};
+				class Specific_city
+				{
+					class Jeonju
+					{
+
+					};
+				
+				};
+				class city
+				{
+					class Iksan
+					{
+
+					};
+					class Gunsan
+					{
+
+					};
+					class Jeongup
+					{
+
+					};
+					class Gimje
+					{
+
+					};
+
+					class Namwon
+					{
+
+					};
+				};
+				class County
+				{
+					class Wanju
+					{
+
+					};
+					class Geochang
+					{
+
+					};
+					class Buan
+					{
+
+					};
+					class Sunchang
+					{
+
+					};
+					class Imsil
+					{
+
+					};
+					class Muju
+					{
+
+					};
+					class Jinan
+					{
+
+					};
+					class Jangsu
+					{
+
+					};
+				};
+			};
+			class Jeoulanamdo
+			{
+				struct Feature
+				{
+				char code[6];
+				
+				double Area;	
+				};
+				class Yeosu
+				{
+
+				};
+				class Mokpo
+				{
+
+				};
+				class Suncheon
+				{
+
+				};
+				class Gwangyang
+				{
+
+				};
+				class Naju
+				{
+
+				};
+				class County
+				{
+					class Muan
+					{
+
+					};
+					class Haenam
+					{
+
+					};
+					class Goheung
+					{
+
+					};
+					class Hwasun
+					{
+
+					};
+					class Yeongam
+					{
+
+					};
+					class Yeongwang
+					{
+
+					};
+					class Wando
+					{
+
+					};
+					class Damyang
+					{
+
+					};
+					class Boseong
+					{
+
+					};
+					class Jangseong
+					{
+
+					};
+					class Jangheung
+					{
+
+					};
+					class Gangjin
+					{
+
+					};
+					class Sinan
+					{
+
+					};
+					class Hampyeong
+					{
+
+					};
+					class Jindo
+					{
+
+					};
+					class Gokseong
+					{
+
+					};
+					class Gurye
+					{
+
+					};
+
+
+				};
+				
+			};
+			
+		};
+		class Youngnam
+		{
+			class Gyeongsangbukdo
+			{
+				struct Feature
+				{
+					char code[6];
+					double Area;
+				};
+				class Specific_city
+				{
+					class Pohang
+					{
+
+					};
+				};
+				class city
+				{
+					class Gumi
+					{
+
+					};
+					class Gyeongsan
+					{
+
+					};
+					class Gyeongju
+					{
+
+					};
+					class Andong
+					{
+
+					};
+					class Gimcheon
+					{
+
+					};
+					class Yeongju
+					{
+
+					};
+					class Sangju
+					{
+
+					};
+					class Yeongcheon
+					{
+
+					};
+					class Mungyeoung
+					{
+
+					};
+
+				};
+				class County
+				{
+					class Chilgok
+					{
+
+					};
+					class Uiseong
+					{
+
+					};
+					class Uljin
+					{
+
+					};
+					class Yecheon
+					{
+
+					};
+					class Cheongdo
+					{
+
+					};
+					class Seongju
+					{
+
+					};
+					class Yeongdeok
+					{
+
+					};
+					class Goryeong
+					{
+
+					};
+					class Bonghwa
+					{
+
+					};
+					class Cheongsong
+					{
+
+					};
+					class Yeongyang
+					{
+
+					};
+					class Ulleung
+					{
+
+					};
+				};
+
+			};
+			class Gyeongsangnamdo
+			{
+				struct Feature
+				{
+					char code[6];
+					double Area;
+				};
+				class Specific_city
+				{
+					class Changwon
+					{
+
+					};
+					class Gimhae
+					{
+
+					};
+				
+				};
+				class city
+				{
+					class Jinju
+					{
+
+					};
+					class Yangsan
+					{
+
+					};
+					class Geoje
+					{
+
+					};
+					class Tongyeong
+					{
+
+					};
+					class Sacheon
+					{
+
+					};
+					class Milyang
+					{
+
+					};
+				};
+				class County
+				{
+						class Haman
+						{
+
+						};
+						class Geochang
+						{
+
+						};
+						class Changnyeong
+						{
+
+						};
+						class Goseong
+						{
+
+						};
+						class Namhae
+						{
+
+						};
+						class Hapcheon
+						{
+
+						};
+						class Hamyang
+						{
+
+						};
+						class Sancheong
+						{
+
+						};
+						class Uiryeong
+						{
+
+						};
+				};
+			};
+		};
+		
+		class Chungchung
+		{
+			class Chungchungbukdo
+			{
+				struct Feature
+				{
+					char code[6];
+					double Area;
+				};
+
+				class specific_city
+				{
+					class Cheongju
+					{
+
+					};
+				};
+				class city
+				{
+					class Chungju
+					{
+
+					};
+					class Jecheon
+					{
+
+					};
+					
+				};
+				class County
+				{
+						class Eumseong
+						{
+
+						};
+						class Jincheon
+						{
+
+						};
+						class Okcheon
+						{
+
+						};
+						class Yeongdong
+						{
+
+						};
+						class Goesan
+						{
+
+						};
+						class Jeungpyeong
+						{
+
+						};
+						class Boeun
+						{
+
+						};
+						class Danyang
+						{
+
+						};
+				};
+			};
+			class Chungchungnamdo
+			{
+				struct Feature{ 
+               char code[6];
+				double Area;	
+                };	
+				class Specific_city
+				{
+					class Cheonan
+					{
+
+					};
+
+				};
+				class city
+				{
+						class Asan
+						{
+
+						};
+						class Seosan
+						{
+
+						};
+						class Dangjin
+						{
+
+						};
+						class Gongju
+						{
+
+						};
+						class Nonsan
+						{
+
+						};
+						class Boryeong
+						{
+
+						};
+						class Gyeryong
+						{
+
+						};
+					
+				
+					};
+					class County
+					{
+						class Hongseong
+						{
+
+						};
+						class Yesan
+						{
+
+						};
+						class Buyeo
+						{
+
+						};
+						class Seocheon
+						{
+
+						};
+						class Taean
+						{
+
+						};
+						class Geumsan
+						{
+
+						};
+						class Cheongyang
+						{
+
+						};
+					};	
+			
+				};
+		
+			};
+		};
+	
+
+	
+	};
+}
+
+class Corea: Integration::Korea_Peninsula
+{
+	public:	
+		class Province
+		{
+			struct Province_Feature
+			{
+				char Language[20]={"MultiLanguage"};
+				int Num_of_Province;
+				long population;
+				double Area;
+				char Politics[20]={"Democracy"};
+				int Industry_Evolution= 5;
+			};
+		};
+};
+
+
+int main()
+{
+	
+std::cout<<"Hello world"<<std::endl;
+
+	
+}
